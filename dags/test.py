9b877884de86925ba3d273b6667f9f29b928d5bd
Wee Liang <weeliangng@hotmail.com>
2025-02-07 14:04:57 +0800

    added first dag file and modified docker compose file

--- dags/test.py
+++ dags/test.py
@@ -1,1 +1,12 @@
-123
+from airflow import DAG
+from datetime import datetime, timedelta
+
+default_args = {
+    'start_date': datetime(2026, 3, 1),  # Make sure this is in the past
+}
+
+dag = DAG(
+    'example_dag',
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+)
